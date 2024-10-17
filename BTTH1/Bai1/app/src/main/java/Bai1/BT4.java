a51b9d91ac45f15562ae7805f3f72a99d6f39e58
thnhDat24 <167805586+thnhDat24@users.noreply.github.com>
2024-10-17 11:30:38 +0700

    Add files via upload

---
+++ BTTH1/Bai1/app/src/main/java/Bai1/BT4.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Bai1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author admin
+ */
+public class BT4 {
+    static boolean isPrime(int n){
+        if(n <= 2)
+            return false;
+        for(int i = 2; i <= Math.sqrt(n); i++){
+            if(n % i == 0)
+                return false;
+        }
+        return true;
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Nhap so: ");
+        int number = sc.nextInt();
+        if(isPrime(number))
+            System.out.printf("%d la so nguyen to", number);
+        else{
+            System.out.printf("%d khong la so nguyen to", number);
+        }
+    }
+}
+
